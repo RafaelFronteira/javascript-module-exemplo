4482ea6b468a2a74c539406057ed02a3c6c318e1
Rafael Fronteira <rafael_fronteira@hotmail.com>
2018-09-27 11:59:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import { data } from './lib.js';
+// import data from './lib.js';  // no caso de ter apenas um export default
+// import { arrayExemplo, stringExplos, jsonExemplo} from './lib.js'; // importando todos os
+
+function init() {
+    console.log('data --> ', data);
+    /*
+    // usando todos os objetos importados
+    console.log('exemplo 1 --> ', arrayExemplo);
+    console.log('exemplo 2 --> ', stringExplos);
+    console.log('exemplo 3 --> ', jsonExemplo);
+    */
+}
+
+
+init();
---
+++ lib.js
@@ -0,0 +1,27 @@
+export const data = [
+        { a: 'teste1'},
+        { a: 'teste2'},
+        { a: 'teste3'},
+        { a: 'teste4'},
+        { a: 'teste5' }
+];
+
+/*
+  // caso queira exportar como default
+  export default [
+        { a: 'teste1'},
+        { a: 'teste2'},
+        { a: 'teste3'},
+        { a: 'teste4'},
+        { a: 'teste5' }
+  ];*/
+
+  /*
+  // exemplo exportando varias coisas
+  export const arrayExemplo = [1, 2, 3, 4];
+  export const stringExplos = "Exemplo"
+  export const jsonExemplo = {
+          "exemplo": "teste!",
+          "valor": 123
+  };
+  */
